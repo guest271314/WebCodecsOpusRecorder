b8f19823d903cadddc89f9deb31a5e3c193770a7
guest271314 <guest271314@gmail.com>
2022-07-17 08:32:17 -0700

    Create WebCodecsOpusRecorder.js

---
+++ WebCodecsOpusRecorder.js
@@ -0,0 +1,130 @@
+class WebCodecsOpusRecorder {
+  constructor(track) {
+    const processor = new MediaStreamTrackProcessor({ track });
+    const metadata = { offsets: [] }, // Opus packet offsets
+      blob = new Blob(),
+      config = {
+        numberOfChannels: 1,
+        sampleRate: 48000,
+        codec: 'opus',
+      };
+    Object.assign(this, { track, processor, metadata, blob, config });
+  }
+  async start() {
+    this.processor.readable
+      .pipeTo(
+        new WritableStream({
+          write: (frame) => {
+            if (frame.numberOfChannels !== this.config.numberOfChannels) {
+              this.config.numberOfChannels = frame.numberOfChannels;
+              this.encoder.configure(config);
+            }
+            this.encoder.encode(frame);
+          },
+          close() {
+            console.log('Processor closed');
+          },
+        })
+      )
+      .catch(console.warn);
+    let first = false;
+    this.encoder = new AudioEncoder({
+      error(e) {
+        console.log(e);
+      },
+      output: async (chunk, { decoderConfig } = {}) => {
+        if (decoderConfig) {
+          decoderConfig.description = btoa(
+            String.fromCharCode(...new Uint8Array(decoderConfig.description))
+          );
+          Object.assign(this.metadata, { decoderConfig });
+          console.log(this.metadata);
+        }
+        const { byteLength, duration } = chunk;
+        this.metadata.offsets.push(byteLength);
+        const ab = new ArrayBuffer(byteLength);
+        chunk.copyTo(ab, { planeIndex: 0 });
+        this.blob = new Blob([this.blob, ab]);
+      },
+    });
+
+    this.encoder.configure(this.config);
+  }
+  async stop() {
+    this.track.stop();
+    await this.encoder.flush();
+    const json = JSON.stringify(this.metadata);
+    const length = Uint32Array.of(json.length); // JSON configuration length
+    this.blob = new Blob([length, json, this.blob], {
+      type: 'application/octet-stream',
+    });
+    console.log(URL.createObjectURL(this.blob));
+    const handle = await showSaveFilePicker({
+      startIn: 'music',
+      suggestedName: `recording.opus.webcodecs`,
+    });
+    const writable = await handle.createWritable();
+    await this.blob.stream().pipeTo(writable);
+  }
+}
+class WebCodecsOpusMediaSource {
+  constructor(source) {
+    this.buffer = source;
+    const view = new DataView(this.buffer);
+    const length = view.getUint32(0, true); 
+    const json = new TextDecoder().decode(
+      new Uint8Array(this.buffer).subarray(4, length + 4)
+    );
+    this.config = JSON.parse(json);
+    console.log(this.config);
+    this.data = new Uint8Array(this.buffer).subarray(json.length + 4);
+    this.index = 0;
+    this.timestamp = 0;
+    this.duration = 60000;
+    this.config.decoderConfig.description = new Uint8Array(
+      [...atob(this.config.decoderConfig.description)].map((s) =>
+        s.charCodeAt()
+      )
+    ).buffer;
+    this.audio = new Audio();
+    this.audio.controls = true;
+    for (const event of [
+      'loadedmetadata',
+      'play',
+      'pause',
+      'waiting',
+      'ended',
+    ]) {
+      this.audio.addEventListener(event, (e) => {
+        console.log(e.type, this.audio.currentTime);       
+      });
+    }
+    document.body.appendChild(this.audio);
+    this.ms = new MediaSource();
+    this.ms.addEventListener(
+      'sourceopen',
+      async () => {
+        URL.revokeObjectURL(this.audio.src);
+        const sourceBuffer = this.ms.addSourceBuffer({
+          audioConfig: this.config.decoderConfig,
+        });
+        sourceBuffer.mode = 'sequence';
+        for (const offset of this.config.offsets) {
+          const eac = new EncodedAudioChunk({
+            type: 'key',
+            timestamp: this.timestamp,
+            duration: this.duration,
+            data: this.data.subarray(this.index, this.index + offset),
+          });
+          await sourceBuffer.appendEncodedChunks(eac);
+          this.timestamp += this.duration;
+          this.index += offset;
+        }
+      },
+      { once: true }
+    );
+    this.audio.src = URL.createObjectURL(this.ms);
+    this.audio.play();
+  }
+}
+export {WebCodecsOpusRecorder, WebCodecsOpusMediaSource};
