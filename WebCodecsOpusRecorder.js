fec3ee9cbea35f76c94484ded883356659363ec9
guest271314 <guest271314@gmail.com>
2024-03-20 19:06:32 -0700

    Update WebCodecsOpusRecorder.js
    - Handle ArrayBuffer from WebCodecs format f32 and AudioBuffer from Web Audio API in write()
    - Handle errors for showSaveFilePicker()
    - Check if this.config.mediaSessionMetadata exists and if true if there are properties to process

--- WebCodecsOpusRecorder.js
+++ WebCodecsOpusRecorder.js
@@ -4,14 +4,13 @@ class WebCodecsOpusRecorder {
       track,
     });
     const metadata = {
-        offsets: [],
-        // Opus packet offsets
+        offsets: [], // Opus packet offsets
       },
       blob = new Blob(),
       config = {
         numberOfChannels: 1,
         sampleRate: 48000,
-        codec: 'opus',
+        codec: "opus",
       };
     this.isConfigured = false;
     Object.assign(this, {
@@ -32,7 +31,7 @@ class WebCodecsOpusRecorder {
               this.config.sampleRate = frame.sampleRate;
               console.log(
                 await AudioEncoder.isConfigSupported(this.config),
-                frame
+                frame,
               );
               this.encoder.configure(this.config);
               this.isConfigured = true;
@@ -40,9 +39,9 @@ class WebCodecsOpusRecorder {
             this.encoder.encode(frame);
           },
           close() {
-            console.log('Processor closed');
+            console.log("Processor closed");
           },
-        })
+        }),
       )
       .catch(console.warn);
     let firstEncodedChunk = false;
@@ -53,7 +52,7 @@ class WebCodecsOpusRecorder {
       output: async (chunk, { decoderConfig } = {}) => {
         if (decoderConfig) {
           decoderConfig.description = btoa(
-            String.fromCharCode(...new Uint8Array(decoderConfig.description))
+            String.fromCharCode(...new Uint8Array(decoderConfig.description)),
           );
           Object.assign(this.metadata, {
             decoderConfig,
@@ -79,29 +78,32 @@ class WebCodecsOpusRecorder {
     console.log(this.track);
     await this.encoder.flush();
     const json = JSON.stringify(this.metadata);
-    console.log('stop', this.metadata);
-    const length = Uint32Array.of(json.length);
-    // JSON configuration length
+    console.log("stop", this.metadata);
+    const length = Uint32Array.of(json.length); // JSON configuration length
     this.blob = new Blob([length, json, this.blob], {
-      type: 'application/octet-stream',
+      type: "application/octet-stream",
     });
     console.log(URL.createObjectURL(this.blob));
-    const handle = await showSaveFilePicker({
-      startIn: 'music',
-      suggestedName: `recording.opus.webcodecs`,
-    });
-    const writable = await handle.createWritable();
-    await this.blob.stream().pipeTo(writable);
+    try {
+      const handle = await showSaveFilePicker({
+        startIn: "music",
+        suggestedName: `recording.opus.webcodecs`,
+      });
+      const writable = await handle.createWritable();
+      await this.blob.stream().pipeTo(writable);
+    } catch (e) {
+      console.warn(e);
+    }
   }
 }
 class WebCodecsOpusPlayer {
-  constructor(source, { type = 'mediaSource' } = {}) {
+  constructor(source, { type = "mediaSource" } = {}) {
     this.buffer = source;
     this.type = type;
     const view = new DataView(this.buffer);
     const length = view.getUint32(0, true);
     const json = new TextDecoder().decode(
-      new Uint8Array(this.buffer).subarray(4, length + 4)
+      new Uint8Array(this.buffer).subarray(4, length + 4),
     );
     this.config = JSON.parse(json);
     console.log(this.config);
@@ -112,69 +114,70 @@ class WebCodecsOpusPlayer {
     this.config.decoderConfig.description = new Uint8Array(
       [...atob(this.config.decoderConfig.description)].map((s) =>
         s.charCodeAt()
-      )
+      ),
     ).buffer;
   }
   async play() {
     this.audio = new Audio();
     this.audio.controls = true;
     const events = [
-      'loadedmetadata',
-      'loadeddata',
-      'canplay',
-      'canplaythrough',
-      'play',
-      'playing',
-      'pause',
-      'waiting',
-      'progress',
-      'seeking',
-      'seeked',
-      'ended',
-      'stalled',
-      'timeupdate',
+      "loadedmetadata",
+      "loadeddata",
+      "canplay",
+      "canplaythrough",
+      "play",
+      "playing",
+      "pause",
+      "waiting",
+      "progress",
+      "seeking",
+      "seeked",
+      "ended",
+      "stalled",
+      "timeupdate",
     ];
     for (const event of events) {
       this.audio.addEventListener(event, async (e) => {
-        if (this.type === 'mediaSource') {
-          if (this.ms.readyState === 'open') {
+        if (this.type === "mediaSource") {
+          if (this.ms.readyState === "open") {
             if (
               this.ms.activeSourceBuffers.length &&
               !this.ms.activeSourceBuffers[0].updating &&
-              e.type === 'timeupdate' &&
+              e.type === "timeupdate" &&
               this.audio.currentTime > 0
             ) {
-              this.ms.activeSourceBuffers[0].timestampOffset = this.audio.currentTime;
+              this.ms.activeSourceBuffers[0].timestampOffset =
+                this.audio.currentTime;
             }
-            if (e.type === 'waiting' && this.audio.currentTime > 0) {
+            if (e.type === "waiting" && this.audio.currentTime > 0) {
               console.log(
                 e.type,
                 this.audio.currentTime,
-                this.ms.activeSourceBuffers[0].timestampOffset
+                this.ms.activeSourceBuffers[0].timestampOffset,
               );
               this.ms.activeSourceBuffers[0].timestampOffset = 0;
               // this.audio.currentTime = 0;
               // this.ms.endOfStream();
             }
-            if (e.type === 'ended') {
+            if (e.type === "ended") {
               // this.audio.currentTime = 0;
             }
-            if (e.type === 'loadedmetadata') {
+            if (e.type === "loadedmetadata") {
               console.log(e.type);
               await this.audio.play();
             }
           }
         } else {
-          if (this.type === 'wav' && e.type !== 'timeupdate') {
+          if (this.type === "wav" && e.type !== "timeupdate") {
             console.log(e.type);
           }
         }
       });
     }
     document.body.appendChild(this.audio);
-    if (this.type === 'mediaSource') {
+    if (this.type === "mediaSource") {
       this.ms = new MediaSource();
-      this.ms.addEventListener('sourceopen', async (e) => {
+      this.ms.addEventListener("sourceopen", async (e) => {
         console.log(e.type);
         URL.revokeObjectURL(this.audio.src);
         const sourceBuffer = this.ms.addSourceBuffer({
@@ -182,10 +185,10 @@ class WebCodecsOpusPlayer {
         });
         console.log(this.ms.activeSourceBuffers);
         sourceBuffer.onupdate = (e) => console.log(e.type);
-        sourceBuffer.mode = 'sequence';
+        sourceBuffer.mode = "sequence";
         for (const offset of this.config.offsets) {
           const eac = new EncodedAudioChunk({
-            type: 'key',
+            type: "key",
             timestamp: this.timestamp,
             duration: !this.index ? 53500 : this.duration,
             data: this.data.subarray(this.index, this.index + offset),
@@ -197,11 +200,12 @@ class WebCodecsOpusPlayer {
       });
       this.audio.src = URL.createObjectURL(this.ms);
     } else {
-      if (this.type === 'wav') {
+      if (this.type === "wav") {
         const wav = new WavAudioEncoder({
           numberOfChannels: this.config.decoderConfig.numberOfChannels,
           sampleRate: this.config.decoderConfig.sampleRate,
         });
+        let start = false;
         const decoder = new AudioDecoder({
           error(e) {
             console.error(e);
@@ -218,7 +222,7 @@ class WebCodecsOpusPlayer {
           },
         });
         console.log(
-          await AudioDecoder.isConfigSupported(this.config.decoderConfig)
+          await AudioDecoder.isConfigSupported(this.config.decoderConfig),
         );
         decoder.configure(this.config.decoderConfig);
         this.index = 0;
@@ -226,7 +230,7 @@ class WebCodecsOpusPlayer {
         this.duration = 60000;
         for (const offset of this.config.offsets) {
           const eac = new EncodedAudioChunk({
-            type: 'key',
+            type: "key",
             timestamp: this.timestamp,
             duration: !this.index ? 53500 : this.duration,
             data: this.data.subarray(this.index, this.index + offset),
@@ -242,14 +246,12 @@ class WebCodecsOpusPlayer {
     }
 
     if (
-      Object.hasOwn(this.config, 'mediaSessionMetadata') &&
+      this.config.mediaSessionMetadata &&
       Object.values(this.config.mediaSessionMetadata).length
     ) {
       navigator.mediaSession.metadata = new MediaMetadata(
-        this.config.mediaSessionMetadata
+        this.config.mediaSessionMetadata,
       );
-    } else {
-      navigator.mediaSession.metadata = null;
     }
   }
 }
@@ -272,17 +274,25 @@ class WavAudioEncoder {
     });
   }
   write(buffer) {
-    const floats = new Float32Array(buffer);
     let channels;
-    // Deinterleave
-    if (this.numberOfChannels > 1) {
-      channels = [[], []];
-      for (let i = 0, j = 0, n = 1; i < floats.length; i++) {
-        channels[(n = ++n % 2)][!n ? j++ : j - 1] = floats[i];
-      }
-      channels = channels.map((f) => new Float32Array(f));
-    } else {
-      channels = [floats];
+    // ArrayBuffer, WebCodecs AudioData f32 format
+    if (buffer instanceof ArrayBuffer) {
+      const floats = new Float32Array(buffer);
+      // Deinterleave
+      channels = this.numberOfChannels === 2
+        ? Object.values(
+          Object.groupBy(floats, (_, i) => i % 2),
+        )
+        : [floats];
+    }
+    // Web Audio API AudioBuffer
+    if (buffer instanceof AudioBuffer) {
+      channels = Array.from(
+        {
+          length: buffer.numberOfChannels,
+        },
+        (_, i) => buffer.getChannelData(i),
+      );
     }
     const [{ length }] = channels;
     const ab = new ArrayBuffer(length * this.numberOfChannels * 2);
@@ -294,7 +304,7 @@ class WavAudioEncoder {
         data.setInt16(
           offset,
           x < 0 ? Math.max(x, -0x8000) : Math.min(x, 0x7fff),
-          true
+          true,
         );
         offset += 2;
       }
@@ -312,10 +322,10 @@ class WavAudioEncoder {
     const dataSize = this.numberOfChannels * this.numberOfSamples * 2;
     const buffer = new ArrayBuffer(44);
     const view = new DataView(buffer);
-    this.setString(view, 0, 'RIFF');
+    this.setString(view, 0, "RIFF");
     view.setUint32(4, 36 + dataSize, true);
-    this.setString(view, 8, 'WAVE');
-    this.setString(view, 12, 'fmt ');
+    this.setString(view, 8, "WAVE");
+    this.setString(view, 12, "fmt ");
     view.setUint32(16, 16, true);
     view.setUint16(20, 1, true);
     view.setUint16(22, this.numberOfChannels, true);
@@ -323,20 +333,20 @@ class WavAudioEncoder {
     view.setUint32(28, this.sampleRate * 4, true);
     view.setUint16(32, this.numberOfChannels * 2, true);
     view.setUint16(34, 16, true);
-    this.setString(view, 36, 'data');
+    this.setString(view, 36, "data");
     view.setUint32(40, dataSize, true);
     this.controller.close();
     return new Blob(
       [
         buffer,
         await new Response(this.readable, {
-          cache: 'no-store',
+          cache: "no-store",
         }).arrayBuffer(),
       ],
       {
-        type: 'audio/wav',
-      }
+        type: "audio/wav",
+      },
     );
   }
 }
-export { WebCodecsOpusRecorder, WebCodecsOpusPlayer, WavAudioEncoder };
+export { WavAudioEncoder, WebCodecsOpusPlayer, WebCodecsOpusRecorder };
